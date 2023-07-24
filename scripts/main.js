ef534b12ea494290f4ad6ba0d58cebbfa7105e95
Asilbek <asilbekpro37@gmail.com>
2023-07-25 00:34:48 +0500

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,2 @@
+!function(){const e=(r=".airlines__slider",Array.isArray(r)?document.querySelectorAll(r):document.querySelector(r));var r;!function(){const r=e.querySelectorAll(".airlines__slider-content"),n=e.querySelector(".airlines__slider-content"),t=window.getComputedStyle(n).animationDuration,i=1e3*parseInt(t);setInterval((()=>{r[0].remove(),e.appendChild(n)}),i)}()}();
+//# sourceMappingURL=main.js.map
